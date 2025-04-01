ea29cdfed0a23be2553fe266d62e2889c6af6538
KarimAbushbak <karimabushbak@gmail.com>
2025-04-01 11:43:15 +0200

    Refactoring the main

--- lib/test.dart
+++ lib/test.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/main
 class Test extends StatelessWidget {
   const Test({super.key});
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
+
     return Scaffold(
       backgroundColor: Colors.red,
     );
-=======
-    return Scaffold();
->>>>>>> origin/main
-  }
-}
+
+}}
